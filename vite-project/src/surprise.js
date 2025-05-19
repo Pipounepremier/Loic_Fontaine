df62413a8c734f9c2b59f7e7a4fb31cb20036534
Loic Fontaine <loic.fontaine@epita.fr>
2025-05-19 11:06:02 +0200

    modif de la page SURPRISE

---
+++ vite-project/src/surprise.js
@@ -0,0 +1,38 @@
+const canvas = document.getElementById('stars');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let stars = [];
+for (let i = 0; i < 150; i++) {
+  stars.push({
+    x: Math.random() * canvas.width,
+    y: Math.random() * canvas.height,
+    r: Math.random() * 1.5 + 0.5,
+    d: Math.random() * 0.5 + 0.2
+  });
+}
+
+function drawStars() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = '#ffffff';
+  stars.forEach(star => {
+    ctx.beginPath();
+    ctx.arc(star.x, star.y, star.r, 0, Math.PI * 2, false);
+    ctx.fill();
+  });
+  moveStars();
+}
+
+function moveStars() {
+  stars.forEach(star => {
+    star.y += star.d;
+    if (star.y > canvas.height) {
+      star.y = 0;
+      star.x = Math.random() * canvas.width;
+    }
+  });
+}
+
+setInterval(drawStars, 50);
+
